a99ae48ebcdcba6d47da2f767552867d8a8c6af1
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-22 15:06:11 +0200

    wip

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*
-### EXAMPLE EXERCISE:
-- Write an Airplane constructor that initializes `name` from an argument.
-- All airplanes built with Airplane should initialize with an `isFlying` of false.
-- Give airplanes the ability to `.takeOff()` and `.land()`:
-      + If a plane takes off, its `isFlying` property is set to true.
-      + If a plane lands, its `isFlying` property is set to false.
+  EXAMPLE EXERCISE:
+  - Write an Airplane constructor that initializes `name` from an argument.
+  - All airplanes built with Airplane should initialize with an `isFlying` of false.
+  - Give airplanes the ability to `.takeOff()` and `.land()`:
+        + If a plane takes off, its `isFlying` property is set to true.
+        + If a plane lands, its `isFlying` property is set to false.
 */
 
 // EXAMPLE SOLUTION CODE:
@@ -26,16 +26,16 @@ Airplane.prototype.land = function () {
 */
 
 /*
-### EXERCISE 1
-  - Build a Person Constructor that initializes `name` and `age` from arguments.
-  - All instances of Person should initialize with an empty `stomach` -> [].
-  - Give instances of Person the ability to `.eat("someFood")`:
-        + When eating an edible, it should be pushed into the `stomach`.
-        + Prevent the `eat` method from pushing more than 10 items into the `stomach`.
-  - Give instances of Person the ability to `.poop()`:
-        + When an instance poops, its `stomach` should empty.
-  - Give instances of Person a method `.toString()`:
-        + It should return a string with `name` and `age`. Example: "Mary, 50"
+  EXERCISE 1
+    - Build a Person Constructor that initializes `name` and `age` from arguments.
+    - All instances of Person should initialize with an empty `stomach` array.
+    - Give instances of Person the ability to `.eat("someFood")`:
+          + When eating an edible, it should be pushed into the `stomach`.
+          + The `eat` method should have no effect if there are 10 items in the `stomach`.
+    - Give instances of Person the ability to `.poop()`:
+          + When an instance poops, its `stomach` should empty.
+    - Give instances of Person a method `.toString()`:
+          + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 function Person(name, age) {
   this.name = name
@@ -45,27 +45,30 @@ function Person(name, age) {
 Person.prototype.eat = function(item) {
   if (this.stomach.length < 10) {
     this.stomach.push(item)
+    return 'yummy'
   }
+  return 'stuffed!'
 }
 Person.prototype.poop = function() {
   this.stomach = []
+  return 'pooping'
 }
 Person.prototype.toString = function() {
   return `${this.name} ${this.age}`
 }
 
 /*
-### EXERCISE 2
-  - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
-  - All instances built with Car:
-        + should initialize with an empty `tank`. -> 0
-        + should initialize with an `odometer` -> 0
-  - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-  - Give cars ability to `.drive(distance)`. The distance driven:
-        + Should cause the `odometer` to go up.
-        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-  - A car which runs out of `fuel` while driving can't drive any more distance:
-        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+  EXERCISE 2
+    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+    - All instances built with Car:
+          + should initialize with an `tank` at 0
+          + should initialize with an `odometer` at 0
+    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    - Give cars ability to `.drive(distance)`. The distance driven:
+          + Should cause the `odometer` to go up.
+          + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+    - A car which runs out of `fuel` while driving can't drive any more distance:
+          + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
 function Car(model, milesPerGallon) {
@@ -90,8 +93,20 @@ Car.prototype.drive = function (distance) {
   }
 }
 
-function Baby() {
-  
+/*
+  EXERCISE 3
+    - Write a Baby constructor subclassing Person.
+    - Besides `name`, Baby takes a second argument to initialize `favoriteToy`.
+    - Besides the methods on Person.prototype, babies have the ability to `.play()`:
+        + Should return a string "Playing with x", x being the favorite toy.
+*/
+function Baby(name, favToy) {
+  Person.call(this, name)
+  this.favoriteToy = favToy
+}
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function () {
+  return `playing with ${this.favoriteToy}`
 }
 
 
--- test/index.test.js
+++ test/index.test.js
@@ -21,35 +21,35 @@ if (typeof exports !== 'undefined') {
 var expect = chai.expect
 
 // Example:
-describe('Airplane', () => {
-  it('instance has name', () => {
+describe('Instances of Airplane', () => {
+  it('initialize with the given name', () => {
     const a = new Airplane('Gulfstream 550')
     expect(a.name).to.eq(`Gulfstream 550`)
   })
-  it('behaves correctly', () => {
-    const myPlane = new Airplane('Jumbo');
+  it('behave correctly', () => {
+    const myPlane = new Airplane('Jumbo')
     expect(myPlane.name).to.equal('Jumbo')
     expect(myPlane.isFlying).to.equal(false)
-    myPlane.takeOff();
+    myPlane.takeOff()
     expect(myPlane.isFlying).to.equal(true)
-    myPlane.land();
+    myPlane.land()
     expect(myPlane.isFlying).to.equal(false)
   })
 })
 
-describe('Person', () => {
+describe('Instances of Person', () => {
   let neo
   const foods = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   beforeEach(() => {
     neo = new Person('Neo', 20)
   })
-  it('initializes with the given name', () => {
+  it('initialize with the given name', () => {
     expect(neo.name).to.equal('Neo')
   })
-  it('initializes with the given age', () => {
+  it('initialize with the given age', () => {
     expect(neo.age).to.equal(20)
   })
-  it('initializes with an empty stomach', () => {
+  it('initialize with an empty stomach', () => {
     expect(neo.stomach).to.be.an('array')
     expect(neo.stomach.length).to.equal(0)
   })
@@ -68,57 +68,76 @@ describe('Person', () => {
     expect(neo.stomach.length).to.equal(0)
   })
   it('can state name and age', () => {
-    const str = neo.toString();
+    const str = neo.toString()
     expect(str).to.include('Neo')
     expect(str).to.include('20')
   })
 })
 
-describe('Car', () => {
+describe('Instances of Car', () => {
   let batmobile
   beforeEach(() => {
     batmobile = new Car('BatMobile', 20)
   })
-  it('initializes with the given model', () => {
+  it('initialize with the given model', () => {
     expect(batmobile.model).to.equal('BatMobile')
   })
-  it('initializes with the given milesPerGallon', () => {
+  it('initialize with the given milesPerGallon', () => {
     expect(batmobile.milesPerGallon).to.equal(20)
   })
-  it('initializes with an empty tank', () => {
+  it('initialize with an empty tank', () => {
     expect(batmobile.tank).to.equal(0)
   })
-  it('initializes with an odometer at 0 miles', () => {
+  it('initialize with an odometer at 0 miles', () => {
     expect(batmobile.odometer).to.equal(0)
   })
-  it('fuel method increases the tank by the given gallons', () => {
+  it('fill method increases the tank by the given gallons', () => {
     batmobile.fill(10)
     expect(batmobile.tank).to.equal(10)
     batmobile.fill(10)
     expect(batmobile.tank).to.equal(20)
   })
-  it('driving when enough fuel increases odometer correctly', () => {
+  it('drive method when enough fuel increases odometer correctly', () => {
     batmobile.fill(10)
     batmobile.drive(50)
     expect(batmobile.odometer).to.equal(50)
   })
-  it('driving when enough fuel decreases tank correctly', () => {
+  it('drive method when enough fuel decreases tank correctly', () => {
     batmobile.fill(10)
     batmobile.drive(100)
     expect(batmobile.tank).to.equal(5)
   })
-  it('driving when NOT enough fuel increases miles by drivable miles', () => {
+  it('drive method when NOT enough fuel increases miles by drivable miles', () => {
     batmobile.fill(10)
     batmobile.drive(201)
     expect(batmobile.odometer).to.equal(200)
   })
-  it('driving when NOT enough fuel empties the tank', () => {
+  it('drive method when NOT enough fuel empties the tank', () => {
     batmobile.fill(10)
     batmobile.drive(201)
     expect(batmobile.tank).to.equal(0)
   })
-  it('driving when NOT enough fuel returns correct string', () => {
+  it('drive method when NOT enough fuel returns correct string', () => {
     batmobile.fill(10)
     expect(batmobile.drive(201)).to.include(200)
+  })
+})
+
+describe('Instances of Baby', () => {
+  let baby
+  beforeEach(() => {
+    baby = new Baby('Lucy', 'trains')
+  })
+  it('initialize with the given name', () => {
+    expect(baby.name).to.equal('Lucy')
+  })
+  it('initialize with the given favorite toy', () => {
+    expect(baby.favoriteToy).to.equal('trains')
+  })
+  it('can play with favorite toy', () => {
+    expect(baby.play()).to.include('trains')
+  })
+  it('inherit the methods on Person.prototype', () => {
+    expect(Person.prototype).to.equal(baby.__proto__.__proto__)
   })
 })
