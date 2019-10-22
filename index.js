63358d6e84776bb79e10202c3be59cb9e45204c7
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-22 12:03:53 +0200

    fix Baby instructions

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function Car() {
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
-    - Besides `name`, Baby takes a second argument to initialize `favoriteToy`.
+    - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
