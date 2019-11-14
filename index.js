6e988a6abadbcd5a1db54114ca7fac2622d7f0a1
Matthew <matthewjamesbedard@outlook.com>
2019-11-13 21:28:48 -0500

    final

--- index.js
+++ index.js
@@ -39,8 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
 
+Person.prototype.eat = function (someFood) {
+  if (this.stomach.length < 10) {
+    return this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function () {
+  return this.stomach = [];
+}
+
+Person.prototype.toString = function () {
+  return (`${this.name} and ${this.age}`)
 }
 
 /*
@@ -57,7 +73,27 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+
+Car.prototype.fill = function (gallons) {
+  this.tank = (this.tank + gallons);
+}
+
+Car.prototype.drive = function (distance) {
+  const maxDistance = this.milesPerGallon * this.tank;
+  if (distance > maxDistance) {
+    distance = maxDistance;
+  }
+  this.odometer += distance;
+  this.tank -= distance / this.milesPerGallon;
+  if (this.tank <= 0) {
+    return (`I ran out of fuel at ${this.odometer} miles`)
+  }
 
 }
 
@@ -68,10 +104,19 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy;
 }
 
+
+
+Baby.prototype = Object.create(Person.prototype)
+
+
+Baby.prototype.play = function () {
+  return (`Playing with ${this.favoriteToy}`)
+}
 /* 
   TASK 4
 
@@ -88,8 +133,16 @@ function Baby() {
 ///////// END OF CHALLENGE /////////
 if (typeof exports !== 'undefined') {
   module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+  if (Airplane) {
+    module.exports.Airplane = Airplane
+  }
+  if (Person) {
+    module.exports.Person = Person
+  }
+  if (Car) {
+    module.exports.Car = Car
+  }
+  if (Baby) {
+    module.exports.Baby = Baby
+  }
 }
