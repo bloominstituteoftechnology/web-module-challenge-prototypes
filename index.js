2f3e7b52c73c5e557757672b61aeff844a50899d
LeiMaya19 <nicholsjanae6@gmail.com>
2019-12-10 13:15:40 -0800

    finished project

--- index.js
+++ index.js
@@ -39,8 +39,22 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.stomach = []
+  this.name = name,
+  this.age = age
+}
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length < 10){
+    return this.stomach.push(someFood)
+  }
+}
+Person.prototype.poop = function() {
+  return this.stomach = []
+}
 
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
 }
 
 /*
@@ -57,10 +71,26 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+ this.model = model
+ this.milesPerGallon = milesPerGallon
+ this.tank = 0
+ this.odometer = 0
 }
-
+Car.prototype.fill = function(gallons) {
+  return this.tank += gallons ;
+}
+Car.prototype.drive = function(distance){}
+Car.prototype.drive = function(distance){
+  if (distance/this.milesPerGallon >= this.tank ){
+    console.log(distance/this.milesPerGallon)
+    this.odometer +=  this.tank*this.milesPerGallon ;
+    this.tank = 0 ;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+  this.odometer += distance ;
+  this.tank -= distance/this.milesPerGallon ;
+}
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -68,18 +98,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+ this.name = name
+ this.age = age
+ this.favoriteToy = favoriteToy
+}
+Baby.prototype = Object.create(Person.prototype);
 
+Baby.prototype.play = function(){
+  return `playing with ${this.favoriteToy}`
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Explicit binding - using call, apply, bind to identify meaning of this.
+  2. Global binding - when this is console logged the value will be the window console object.
+  3. New binding - a constructor function is used 'this' is the object created and return by the constructor function.
+  4. Implicit bining - a function is called the object left of the dot gets the the value of 'this'
 */
 
 
