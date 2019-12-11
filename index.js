c7e49090545a09ce68b8f21934e906dd686fe54e
Jose <pjose14.jp38@yahoo.com>
2019-12-10 22:03:20 -0600

    almost done with the challenges. need to finish stretch, task 3 and explain the four principals of this

--- index.js
+++ index.js
@@ -39,8 +39,27 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name,
+  this.age = age,
+  this.stomach = []
+}
+Person.prototype.eat = function(eat) {
+  if(this.stomach.length < 10){
+    this.stomach.push(eat);
+  } else {
+    return this.stomach;
+  }
+}
+Person.prototype.poop = function(poop) {
+  if(this.stomach.length = 0) {
+    function empty(stomach) {
+      stomach = [];
+    }
+  }
+}
+Person.prototype.toString = function(){
+  return `${this.name} ${this.age}`;  
 }
 
 /*
@@ -57,8 +76,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer= 0;
+}
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
 }
 
 /*
@@ -68,9 +93,13 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
+
+
 
 /* 
   TASK 4
