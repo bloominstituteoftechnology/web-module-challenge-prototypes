4baaf56ed1757ded3a0d89f3253cc0104b5debd3
Robinson86 <56929152+Robinson86@users.noreply.github.com>
2019-12-14 15:18:43 -0500

    done

--- index.js
+++ index.js
@@ -39,9 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+  
 }
+Person.prototype.eat = function(someFood){
+  if ( this.stomach.length < 10){
+    this.stomach.push(someFood);
+  } 
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function (){
+  return `${this.name}, ${this.age}`;
+}
+
+
+
 
 /*
   TASK 2
@@ -57,8 +74,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function (gallons){
+   this.tank += gallons;
 }
 
 /*
@@ -68,18 +91,27 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
-}
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+  }
+  Baby.prototype = Object.create(Person.prototype);
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`;
+  }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+ 1. Window Binding - if no other rules apply, then this defaults to window object.
+
+  2.Implicit Binding - when function is invoked look to the left of the dot to see what the this word is referring to.
+
+  3.Explicit Binding - uses call, apply, and bind to explicitly states what this is referring to in any given function.
+
+  4. New Binding - uses the new keyword creates a new object and the this keyword points to it.
 */
 
 
