77d93e932eb4ef40964cb251898acac56fa0a389
Mandi Haase <haase1020@gmail.com>
2019-12-18 16:32:17 -0500

    answer question 2

--- index.js
+++ index.js
@@ -73,8 +73,20 @@ const neo = new Person({
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
 
+  this.tank = this.tank + gallons;
+}
+
+Car.prototype.drive = function (distance, milesPerGallon){
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - this.milesPerGallon;
 }
 
 /*
