f822aefaf81b7bc63b95216ed3f2f15b7c11fa11
matthew-heideman <heidemanmr@yahoo.com>
2020-01-29 23:43:21 -0600

    Completed JS 3 with completed stretch

--- index.js
+++ index.js
@@ -76,9 +76,17 @@ function Car(model, milesPerGallon) {
  this.odometer = 0
 }
 Car.prototype.fill = function(gallons){
-  return this.tank += gallons;
+  this.tank += gallons;
 }
-
+Car.prototype.drive = function(distance){
+  this.odometer += distance;
+  this.tank -= distance / this.milesPerGallon;
+  if(this.tank < 0){
+    this.odometer -= Math.abs(this.tank) * this.milesPerGallon;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+}
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
