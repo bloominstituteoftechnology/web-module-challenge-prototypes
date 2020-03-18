b33986ffd012b675c813fc44e3a6e9a34dc3d709
Carlos Ernesto Vazquez <mundo_vacio@yahoo.com>
2020-03-17 21:02:55 -0700

    JS-Exercise-Prototype

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ In order to complete these tasks you will need your newly aquired knowledge of c
 
 ##### Index
 
-* [Instructions](#instructions)
-* [Get Started](#get-started)
+- [Instructions](#instructions)
+- [Get Started](#get-started)
 
 ## Instructions
 
 Find the file `index.js` and complete tasks 1, 2 and 3 until all of your tests pass.
 There is an additional task 4 which requires written explanations and has no tests.
 
-If you run into trouble while coding, fight the good fight for 20 minutes and then get on the help channel. __Remember to formulate your help request in a professional manner__ - like you would at the job - by including error messages, screenshots, and any other pertinent information about the problem, as well as what things you have attempted already while trying to solve it.
+If you run into trouble while coding, fight the good fight for 20 minutes and then get on the help channel. **Remember to formulate your help request in a professional manner** - like you would at the job - by including error messages, screenshots, and any other pertinent information about the problem, as well as what things you have attempted already while trying to solve it.
 
 ## Get Started
 
@@ -21,13 +21,13 @@ Two options are included below.
 <details>
   <summary>1. Using Code Sandbox</summary>
 
-  * Launch the sandbox using the link below.
-  * Sign into Code Sandbox.
-  * Fork the sandbox.
-  * See your tests running on the "Browser" tab (NOT the "Tests" tab).
-  * The way you'll submit your work will be by pasting a link to your fork into the submission form.
+- Launch the sandbox using the link below.
+- Sign into Code Sandbox.
+- Fork the sandbox.
+- See your tests running on the "Browser" tab (NOT the "Tests" tab).
+- The way you'll submit your work will be by pasting a link to your fork into the submission form.
 
-  [LAUNCH ON CODESANDBOX 🚀](https://codesandbox.io/s/github/LambdaSchool/JS-Exercise-Prototype?previewwindow=browser)
+[LAUNCH ON CODESANDBOX 🚀](https://codesandbox.io/s/github/LambdaSchool/JS-Exercise-Prototype?previewwindow=browser)
 
   <img alt='instructions Code Sandbox' src='./instructionsCodeSandbox.png'>
 </details>
@@ -35,14 +35,14 @@ Two options are included below.
 <details>
   <summary>2. Using VSCode and the Command Line</summary>
 
-  1. Fork repo and add TL as collaborator on Github.
-  1. Clone _your_ fork (not Lambda's repo by mistake!).
-  1. `cd` into your newly cloned repository.
-  1. Create a new branch by typing `git checkout -b <firstName-lastName>`.
-  1. Install dependencies by typing `npm install`.
-  1. Run tests by typing `npm run test:watch`.
-  1. Work on your branch, push commits and create PR as usual.
-  1. Make sure to commit often!!
+1. Fork repo and add TL as collaborator on Github.
+1. Clone _your_ fork (not Lambda's repo by mistake!).
+1. `cd` into your newly cloned repository.
+1. Create a new branch by typing `git checkout -b <firstName-lastName>`.
+1. Install dependencies by typing `npm install`.
+1. Run tests by typing `npm run test:watch`.
+1. Work on your branch, push commits and create PR as usual.
+1. Make sure to commit often!!
 
   <img alt='instructions VSCode' src='./instructionsVScode.png'>
 </details>
--- index.js
+++ index.js
@@ -12,13 +12,12 @@ function Airplane(name) {
   this.name = name;
   this.isFlying = false;
 }
-Airplane.prototype.takeOff = function () {
+Airplane.prototype.takeOff = function() {
   this.isFlying = true;
 };
-Airplane.prototype.land = function () {
+Airplane.prototype.land = function() {
   this.isFlying = false;
 };
-
 
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -39,9 +38,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(food) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(food);
+  }
+};
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+};
 
 /*
   TASK 2
@@ -57,9 +72,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+};
+
+Car.prototype.drive = function(distance) {
+  this.odometer += distance;
+  const gallonsNeeded = distance / this.milesPerGallon;
+  this.tank -= gallonsNeeded;
+
+  if (this.tank <= 0) {
+    this.odometer -= this.milesPerGallon * Math.abs(this.tank);
+    this.tank = 0;
+    return `I ran out of gas at ${this.odometer} miles!`;
+  }
+};
 
 /*
   TASK 3
@@ -68,28 +102,47 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+                            //What is the calling object? 
+  (Default Binding || Implicit Binding || Explicit Binding || New Binding)
+                      
+  1. Is the function called by new?
+  2. Is the function called by call(), apply(), or bind()?
+  3. Is the function called as a method, ie: obj.func()?
+  4. Is the function called in the global scope?
+     If strict mode is enabled, return undefined.
+     Otherwise, return the global object, ie: window.
 */
 
-
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
-if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+if (typeof exports !== "undefined") {
+  module.exports = module.exports || {};
+  if (Airplane) {
+    module.exports.Airplane = Airplane;
+  }
+  if (Person) {
+    module.exports.Person = Person;
+  }
+  if (Car) {
+    module.exports.Car = Car;
+  }
+  if (Baby) {
+    module.exports.Baby = Baby;
+  }
 }
