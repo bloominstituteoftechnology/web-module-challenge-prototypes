44fed02de368cf468603a837702ce00c12fa801c
JenVest2020 <jenvest2019@gmail.com>
2020-04-28 21:23:25 -0500

    passed ALL Baby constructor tasks

--- index.js
+++ index.js
@@ -84,8 +84,16 @@ Car.prototype.fill = function fill(gallons) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age, favoriteToy);
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
 
+Baby.prototype.play = function play(favoriteToy) {
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
