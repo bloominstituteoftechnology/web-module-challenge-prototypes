f13412956135caeb262e8fdf0ad1488e60a74bd4
Aaron Yates <aaronmyates@hotmail.com>
2020-04-30 22:41:31 -0400

    I'm terrible at keeping up with this

--- index.js
+++ index.js
@@ -39,9 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function (someFood){
+  if(this.stomach.length < 10)
+  {
+    this.stomach.push(someFood);
+  }
+};
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+};
+
+Person.prototype.toString = function (){ return this.name + ", " + this.age};
 
 /*
   TASK 2
@@ -57,8 +72,29 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance){
+  var maxdistance = this.tank * this.milesPerGallon;
+
+  if(distance <= maxdistance){
+    this.odometer += distance;
+    this.tank -= (distance/this.milesPerGallon);
+  }
+  else{
+    this.odometer += maxdistance;
+    this.tank = 0;
+    return "I ran out of fuel at " + this.odometer + " miles!";
+  }
 }
 
 /*
@@ -68,18 +104,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return "Playing with " + this.favoriteToy;
+};
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. The first principle of "this" is global and/orwindow object binding. Without a preceding dot call "this" will be the window or the console
+  2. The second principle is implicit binding. Whatever is calling "this" with dot notation will be bound to "this"
+  3. the third is the principle of new object binding. When instantiating a new object "this" refers to the object being instanced by the new keyword.
+  4. the last principle is one of explicit binding. Using the call or apply keyword we can explicitly direct "this" to point to another object.
 */
 
 
