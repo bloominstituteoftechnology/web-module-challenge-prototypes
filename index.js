c8dc831c7133258fe704775f71b8155223bdcbcd
chqiu6 <chucky-qiu@lambdastudents.com>
2020-05-14 17:01:00 -0400

    completed except stretch

--- index.js
+++ index.js
@@ -39,9 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length < 10) {
+    return this.stomach.push(someFood);
+  }
+};
+
+Person.prototype.poop = function() {
+  this.stomach.splice(0, this.stomach.length);
+};
+
+Person.prototype.toString = function() {
+  return `Name: ${this.name}, Age: ${this.age}`;
+};
 
 /*
   TASK 2
@@ -57,9 +72,17 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car("Tesla", 20);
+Car.prototype.fill = function(gallons) {
+  return (this.tank += gallons);
+};
+
 
 /*
   TASK 3
@@ -68,18 +91,24 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Baby is playing with ${this.favoriteToy}`;
+};
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/global binding = will be outside the functino
+  2. implicit binding = similar to dot notation "this" is before the object
+  3. explicit binding = uses call or apply in js 
+  4. new binding = this is being referenced when the object was created in the counstructor function
 */
 
 
