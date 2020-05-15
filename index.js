48e8a8c0b1d069d8f4abfd58a2ee98dfcb16aa5c
unknown <clay2000lbs@gmail.com>
2020-05-14 19:12:06 -0500

    n/a

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(edible) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
+};
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function() {
+  return `${name}, ${age}`;
+};
 
 /*
   TASK 2
@@ -57,9 +73,40 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 
 }
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+};
+
+Car.prototype.drive = function(distance) {
+  if (this.tank - distance / this.milesPerGallon <= 0) {
+
+    for (let i = distance; i > 0; i--) {
+
+      if (this.tank - i / this.milesPerGallon === 0) {
+
+        this.odometer += i;
+        this.tank = 0;
+
+        return `I ran out of fuel at ${this.odometer} miles!`;
+
+      }
+    }
+  } else {
+
+    this.tank -= distance / this.milesPerGallon;
+    this.odometer += distance;
+    
+  }
+};
 
 /*
   TASK 3
@@ -68,18 +115,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. window- this will refer to the console/ all of global JS
+  2. implicit- this will refer to w/e is being called by a method
+  3. new- 
+  4. explicit-
 */
 
 
