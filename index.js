300c06c3713d5345c2e1074e3df6151f31db02b9
Aaron Flowers <flowersa29@gmail.com>
2020-06-11 21:18:28 -0500

    6/11

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  
 
 }
 
