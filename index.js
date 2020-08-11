903961937ad16483d4a821857f8aee6e72bfc065
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-08-11 09:02:22 -0700

    Call for inheritance and bind for functions

--- index.js
+++ index.js
@@ -39,28 +39,66 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+
+
+
 
 }
 
+Person.prototype.eat = function(food){
+  /// EETING FOOOD
+  this.stomach.map(function(e){
+    e.push(food);
+  });
+
+
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  let l = this.name + ' ' + this.age;
+  return l;
+}
+
+let p = new Person('Floyd',244);
+console.log(p);
+p.eat('ffooood');
+p.poop();
+console.log(p.toString());
 /*
   TASK 2
-    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+    - Write a Car constructor that initializes `model` and `milesPerGallon`
+     from arguments.
     - All instances built with Car:
         + should initialize with an `tank` at 0
         + should initialize with an `odometer` at 0
-    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    - Give cars the ability to get fueled with a `.fill(gallons)` method. 
+    Add the gallons to `tank`.
+    
     - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
     - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
-        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+        + The `drive` method should return a string "I ran out of fuel at x miles!" x being 
+        `odometer`.
 */
 
-function Car() {
+function Car(model,milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
 
 }
 
+Car.prototype.fill  = function(gallons){
+  this.tank =+ gallons;
+}
+let car = new Car('Bishop Emmet', 122);
+car.fill(20);
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
