b0f7f38f9325b8d3eab0a28d3095f73c387a5e54
unknown <erickvillegasecheverria@gmail.com>
2020-08-11 20:22:51 -0700

    MVP + All STRETCHES

--- index.js
+++ index.js
@@ -39,8 +39,21 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function (food){
+  if (this.stomach.length < 10){
+    this.stomach.push(food);
+  }
+}
+Person.prototype.poop = function (){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name},${this.age}`;
 }
 
 /*
@@ -57,8 +70,26 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model,milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function (gallons){
+  this.tank+=gallons;
+}
+Car.prototype.drive = function (distance){
+  let fuelneeded = distance/this.milesPerGallon;
+  if (this.tank >= fuelneeded){
+    this.odometer+=distance;
+    this.tank-=fuelneeded;
+  }
+  else {
+    this.odometer+=(this.milesPerGallon*this.tank);
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
 }
 
 /*
@@ -68,18 +99,44 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name,age,favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function (){
+  return `Playing with ${this.favoriteToy}`;
+}
+
+
+// function Baby(attributes){
+//   Person.call(this, attributes);
+//   this.favoriteToy = attributes.favoriteToy;
+//   this.name = attributes.name;
+//   this.age = attributes.age;
+// }
+
+
+// Baby.prototype.play = function (){
+//   return `${baby1.name}`;
+// }
+
+// const baby1 = new Baby({
+//   name: 'Erick',
+//   age: 1,
+//   favoriteToy: 'videogames',
+// })
+
+// console.log(baby1);
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. When defined globally, "this" referenced the console or window where is being used.
+  2. Implicite means it will refereced the object where it's beign used.
+  3. New binding means that the instance of the constructor is beign referenced. 
+  4. Explicitly it will take the instance of the parent constructor.
 */
 
 
