15101a4e5d6c1b21a4a69cb64e73256726e54fea
JohnathanBarnes <johnathan.f.barnes@gmail.com>
2020-08-13 07:42:44 -0500

    MVP

--- index.js
+++ index.js
@@ -39,9 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.sayName = function () {
+  console.log(this.name);
+};
+
+Person.prototype.eat = function (someFood) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood);
+  }
+};
+Person.prototype.poop = function () {
+  this.stomach = [];
+};
+Person.prototype.toString = function () {
+  return `My name is ${this.name}, and I'm ${this.age} years old.`;
+};
 
 /*
   TASK 2
@@ -57,9 +74,20 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon, tank, odometer) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons;
+};
+car.prototype.fill = function (milesDriven) {
+  const gasTank = Math.round((this.tank - milesDriven) / this.milesPerGallon);
+  const milesToGo = this.tank * this.milesPerGallon;
+};
+
 
 /*
   TASK 3
@@ -68,18 +96,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. In the global, "this" is the window.
+  2. usually in reference to an object.
+  3. Can be used as a method
+  4. used in a new contructor
 */
 
 
