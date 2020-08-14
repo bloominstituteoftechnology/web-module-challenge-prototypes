7153da632a3dd0a0774f729ac913c1a6d3c97322
Andre Orellana <andre@andres-mbp.home>
2020-08-14 15:03:30 -0700

    Finished all tasks

--- index.js
+++ index.js
@@ -135,10 +135,11 @@ console.log(bigBaby.play())
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1.
-  2.
-  3.
-  4.
+  1. Window binding refers to the global object when "this" is not in a function scope
+  2. Implicit Binding is when the "this" keyword is calling the object that is to the left of the dot
+  3. Explicit binding allows you to use "call" or "apply" and immediately invoke the function whereas bind
+  only creates a new function and must be invoked later
+  4. New binding lets you apply a new function name to an already existing one
 */
 
 
