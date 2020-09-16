7c30e3796ca08c59568d528f5b1241f6520d826c
Sam Singer <sam.singet79@gmail.com>
2020-09-16 13:51:41 -0700

    sam-singer

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ Airplane.prototype.takeOff = function () {
 Airplane.prototype.land = function () {
   this.isFlying = false;
 };
-
+const isItFlying = new Airplane('Delta')
+console.log(isItFlying)
 
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -39,10 +40,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.stomach = []
+  this.name = name;
+  this.age = age;
 }
-
+Person.prototype.eat = function(food){
+  if(this.stomach.length <10){
+  this.stomach.push(food)
+  } else{
+    return 'I Am Full'
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = []
+}
+Person.prototype.toString= function(){
+  return `${this.name} and ${this.age}`
+}
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -57,8 +72,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.milesPerGallon = milesPerGallon
+  this.model = model
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  return this.tank += gallons
 }
 
 /*
