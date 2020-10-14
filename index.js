3d244062ed75dd3705dfbb8efe70a92f905ff957
Cameron Mirza <cmirza@Camerons-MBP.lan>
2020-10-14 14:54:59 -0700

    MVP complete

--- index.js
+++ index.js
@@ -39,8 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age){
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
 
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length<10){
+    this.stomach.push(someFood)
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach=[];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -57,8 +73,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon){
+  this.model=model;
+  this.milesPerGallon=milesPerGallon;
+  this.tank=0;
+  this.odometer=0;
+}
 
+Car.prototype.fill = function(gallons){
+  this.tank+=gallons;
 }
 
 /*
@@ -68,18 +91,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
 
+function Baby(name, age, favoriteToy){
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+}
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/global object binding, when in the global scope, the value of 'this' whill be the Window/Console object.
+  2. Implicit binding, when a function is called, the object left of the '.' gets 'this'.
+  3. Explicit binding, when call/apply method is used, 'this' is explicitly defined.
+  4. New binding, when using a constructor function, 'this' refers to the object created by the constructor function.
 */
 
 
