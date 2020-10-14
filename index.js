9ce129d658d12089feae21d6204bd2b096a0528d
de_gentlem4n <th3g3n3lm4n@gmail.com>
2020-10-14 17:29:44 -0400

    finished tasks doing the task 4

--- index.js
+++ index.js
@@ -74,10 +74,20 @@ Person.prototype.toString = function (){
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons;
+}
+Car.prototype.drive = function (distance){
+  this.odometer += distance;
+
+}
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -85,16 +95,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name,age,favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy;
+}
+// inherits method from person
+Baby.prototype = Object.create(Person.prototype);
 
+Baby.prototype.play = function () {
+  return ` Playing with ${this.favoriteToy}`
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
+  1. whenever the function is contained inside the global scope the 'this' keyword will be the "window object"
+  2. to accass and object within the window the function needs to have a dot after the 'this' keyword and an object that you want to access after that dot
   3. 
   4. 
 */
