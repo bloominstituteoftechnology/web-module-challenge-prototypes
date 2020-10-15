c4aff9a8576e2e7e18dd8733cd3add15744ab488
lili <jordanfuchs05@yahoo.com>
2020-10-14 21:51:35 -0700

    finished prototype project

--- index.js
+++ index.js
@@ -39,9 +39,37 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age){
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+const personOne = new Person("Jordan", 25);
+personOne.eat('Pork Bun');
+personOne.eat('ribs');
+
+console.log(personOne.stomach);
+
+personOne.poop();
+
+console.log(personOne.toString());
+
+
 
 /*
   TASK 2
@@ -57,9 +85,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function(gallons) {
+  this.tank = this.tank + gallons;
+}
+
 
 /*
   TASK 3
@@ -68,18 +103,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function (){
+return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Binding - if we dont give 'this' any context it will just think we are referancing the global object (the entire code of javascript)
+  2. implicit binding - applies to object with methods, 'this' to whats left of the dot when the function is invoked.
+  3. explicit binding - using .call(), .apply() and .bind() to specifically say what 'this' is referring to
+  4. new binding - creates a new object and 'this' references that new object created
 */
 
 
