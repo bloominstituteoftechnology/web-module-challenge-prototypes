b0fa6434e69719ecfbc9d2a157e29b2c660c688a
Ahmed alsheikh <ahmedjamal@Ahmeds-MacBook-Pro.local>
2020-10-14 23:34:50 -0700

    all the tasks passed

--- index.js
+++ index.js
@@ -39,9 +39,32 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
 
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(edible)
+{
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+const personOne = new Person('Elijah', 21);
+const persontwo = new Person('Alan', 65);
+
+console.log(personOne.stomach);
 
 /*
   TASK 2
@@ -57,9 +80,29 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons){
+    this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance){
+  if (this.milesPerGallon * this.tank >= distance){
+    this.odometer += distance;
+    this.tank -= distance / this.milesPerGallon;
+  } else {
+    let canDrive = this.milesPerGallon * this.tank;
+    this.tank = 0;
+    this.odometer += canDrive;
+    return `I ran out of fuel at ${this.odometer}!`
+  }
+}
+
 
 /*
   TASK 3
@@ -68,8 +111,13 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name,age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
