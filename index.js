5847807a1472c5e3147208f802fdfadd07d835b1
Trevor Locke <locke.trevor@gmail.com>
2020-11-03 20:16:10 -0600

    MVP

--- index.js
+++ index.js
@@ -39,10 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	this.stomach = [];
 }
 
+Person.prototype.eat = function (someFood) {
+	if (this.stomach.length < 10) {
+		this.stomach.push(someFood);
+	}
+};
+
+Person.prototype.poop = function () {
+	this.stomach = [];
+};
+
+Person.prototype.toString = function () {
+	return `${this.name}, ${this.age}`;
+};
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -57,9 +72,23 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+	this.model = model;
+	this.milesPerGallon = milesPerGallon;
+	this.tank = 0;
+	this.odometer = 0;
 }
+
+Car.prototype.fill = function (gallons) {
+	this.tank += gallons;
+};
+
+Car.prototype.drive = function (distance) {
+	this.odometer += distance;
+	this.tank -= distance;
+};
+
+
 
 /*
   TASK 3
@@ -68,18 +97,27 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+	this.name = name;
+	this.age = age;
+	this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function () {
+	return `Playing with ${this.favoriteToy}.`;
+};
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. global - console or window binding
+  2. implicit- whatever is to the left is the binding
+  3. explicit- .call .apply or .bind invoking
+  4. new- a new object is created and referenced by the this keyword
 */
 
 
