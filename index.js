63cf7dc14c1be90f9fb8da00431be094a0f1bc4d
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-11-04 21:02:04 -1000

    updated all tasks on challenge
    Due to my grandmother passing this afternoon i was unable to get all of the tests to pass the tester but all test code checks out aside from setting a cap on the fuel tank. I will catch that up tomorrow.

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ Airplane.prototype.land = function () {
   this.isFlying = false;
 };
 
-
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -39,9 +38,57 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(attr) {
+                           // declaration of constructor taking attr as para
+  this.name = attr.name;   // assign this.person => attr.person
+  this.age = attr.age;     // assign this.age => attr.person
+  this.stomach = [];       // create empty stomach container
 }
+
+// Working test code for constructor
+// const tony = new Person({ name: "Tony", age: 46 });
+// console.log(tony)
+// output {name: 'Tony', age: 46}, stomach: []};
+
+// EAT PROTOTYPE
+Person.prototype.eat = function (
+  array //prototype function declaration to //intake food
+) {
+  this.stomach = [array]; //puts food in the belly
+};
+//EAT TEST CODE
+// tony.eat([
+//   "rice",
+//   "beans",
+//   "avocado",
+//   "cotija",
+//   "tortilla",
+//   "pico de gallo",
+//   "crema",
+//   "margarita"
+// ]);
+// console.log(tony.stomach[0]);
+// output tony with above contents in stomach
+
+//POOP PROTOTYPE
+Person.prototype.poop = function ()             // prototype declaration of poop
+{
+  this.stomach[0] = [];                         // slicing data starting 0 by stomach.length
+};
+// WORKING POOP TEST CODE
+// tony.poop();
+// console.log(tony.stomach[0]);
+// output of stomach = []
+
+//.toString PROTOTYPE
+Person.prototype.stringify = function ()      // prototype declaration stringify
+{
+  return `${this.name}, ${this.age}`;         // return name, age
+};
+// WORKING .toSting TEST CODE
+// let test = tony.stringify();
+// console.log(test);
+// output of Tony, 46
 
 /*
   TASK 2
@@ -57,9 +104,31 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(car, mpg) {
+                                     // Car constructor declaration passing car and miles per gallon as parameters
+  this.model = car;                  // key this model giving car as value
+  this.milesPerGallon = mpg;         // key this.mpg passing milesPerGallon as value
+  this.tank = 0;                     // initialization of empty tank
+  this.odometer = 0;                 // intitialization of - odometer
 }
+
+//CREATION OF newCar TEST CODE
+// const ferrari = new Car(    "Ferrari",  30    );
+// console.log(ferrari);
+// console.log(`This ${ferrari.model} has ${ferrari.tank} gallons of gas in it`)
+// OUTPUT Car {model: :"Ferrari", mpg: 30, tank: 0, odometer; 0}
+
+// CAR FUELING PROTOTYPE
+Car.prototype.fuel = function (
+  gallons                                               // declaration of .fuel prototype taking gallons as a parameter
+) {
+  this.tank = gallons;                                  // sets this.tank = gallons
+};
+
+// WORKING TEST CODE FUELING THE TANK
+// ferrari.fuel(12)
+// console.log(ferrari)
+// output object with tank = to argument of 12 passed in fuel function call
 
 /*
   TASK 3
@@ -68,18 +137,36 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+f; // BABY MAKER SUBCLASSING Person
+function Baby(name, age, favoriteToy) {
+                                                      //declaring function giving adding  name, age, and favorite toy as parameters
+  Person.call(this, name, age);                       // Binder using this name and age with person
+  this.favoriteToy = favoriteToy;                     // sets this.favorite toy to favoriteToy parameter
 
+  Baby.prototype = Object.create(Person.prototype);   // creates baby using person method
+  Baby.prototype.play = function () {
+                                                      // declaring play function returning expected output statement
+    return `Playing with ${this.favoriteToy}`;
+  };
 }
+
+//WORKING BABY TEST CODE
+const jordan = new Baby({
+  name: "Jordan",
+  age: "2 months",
+  favoriteToy: "Rattle Booties"
+});
+console.log(jordan);
+console.log(jordan.play());
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global/ Window Binding - in global space you are accessing the immediate container which could be the window or the console.
+  2. Implicit Binding - implicit binding is used in methods within objects and refer to the object container
+  3. Explicit Binding - using .call and apply you can override the constructor objects by using the object's oriented (OOP) approach
+  4. "new" keyword binding - keyword binding using 'new' creates new instances of the constructor function under teh given variable name. each time a new instance is created new functionName binds the newly created instance to the constructor.
 */
 
 
