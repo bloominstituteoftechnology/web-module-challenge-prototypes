d626059f3964de332532948676eb160fa997cca4
miketillman95 <matillman95@gmail.com>
2020-11-04 22:59:22 -0800

    hmwk

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = []
 }
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length < 10){
+    this.stomach.push(someFood)
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach= []
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
+}
+let mary = new Person("Mary", 20)
+mary.eat("banana")
+console.log(mary.toString())
 
 /*
   TASK 2
@@ -57,8 +73,23 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
+  this.model= model;
+  this.milesPerGallon = milesPerGallon
 
+
+}
+Car.prototype.fill = function(gallons){
+   this.tank = this.tank + gallons
+   return this.tank
+  
+
+
+}
+Car.prototype.drive = function(distance){
+  if(distance.length > this.odometer){}
 }
 
 /*
@@ -68,18 +99,27 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  // this line is subclassing the first function, which means put it inside the scope
+  Person.call( name, age);
+  this.favoriteToy = favoriteToy
 }
+// this is creating the baby prototype using the Object.keys() method  to returan an array of a given objects own enumerable property names
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function () {
+  return `Playing with his ${this.favoriteToy}`
+}
+  
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1.  when in the global scope, the value of "this" will be the window/console object
+  2. implicit binding lets you call function without using the dot notation
+  3. constructors functions which are object creators 
+  4. use explicit binding which can overridde how we set constructor objects 
 */
 
 
