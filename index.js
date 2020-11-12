02fb8316ab130840a009d548d1fa88d8fa3314b5
William Dye <williamdye@Williams-MacBook-Pro.local>
2020-11-11 23:24:39 -0500

    Final push

--- index.js
+++ index.js
@@ -147,18 +147,24 @@ Car.prototype.drive = function(distance) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+} 
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. this will check if the function is called by new
+  2. this will check is the function is called by call(), apply(), or bind
+  3. this will check if the function is called as a method
+  4. this will check if the function is called in global scope
 */
 
 
