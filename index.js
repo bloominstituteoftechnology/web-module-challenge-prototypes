63b77ba643f33c5481378b57384c03ad994727dd
lili <jordanfuchs05@yahoo.com>
2020-11-11 23:00:20 -0800

    will do stretch when youtube comes back up

--- index.js
+++ index.js
@@ -39,9 +39,36 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+const personOne = new Person("Jordan", 25);
+personOne.eat('Bagel');
+personOne.eat('Cereal');
+
+console.log(personOne.stomach);
+
+personOne.poop();
+
+console.log(personOne.toString());
+
 
 /*
   TASK 2
@@ -57,8 +84,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 
+}
+
+Car.prototype.fill = function(gallons) {
+  this.tank = this.tank + gallons;
 }
 
 /*
@@ -68,18 +103,24 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+}
 
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function (){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Binding - without context "this" refers to global object
+  2. Implicit Binding - applies 'this' to whats left of the dot when the function is invoked
+  3. Explicit Biniding - using .apply(), .call(), and .bind() to specify what 'this' is referring to
+  4. New Binding - creates new object and refers to it using 'this'
 */
 
 
