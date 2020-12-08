ca4d16b1b136990fdade8d13305d001df2298ed3
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2020-12-08 15:28:46 -0500

    MVP

--- index.js
+++ index.js
@@ -39,12 +39,26 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name
+    this.age = age
+    this.stomach = [];
   }
- 
- 
-
+  Person.prototype.eat("some food") = function (edible) {
+    if(this.stomach.length < 10){
+      this.stomach.push(edible)
+    }
+  };
+  Person.prototype.poop = function () {
+    this.stomach = [];
+  };
+ Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+const PersonUno = new Person ({ 
+name: "Billy" , 
+age: 37
+})
   
   
   
@@ -63,10 +77,18 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.tank = 0;
+    this.odometer = 0; 
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
   }
-  
+  Car.prototype.fill = function(gallons){
+      this.tank += gallons;}
+
+  const CarUno = new Car('Subaru', 30);
+
+  carOne.fill(30)
   
   /*
     TASK 3
@@ -75,18 +97,28 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
- 
-  
+ function Baby(name,age, favoriteToy) {
+  Person.call(this, name,age,favoriteToy);
+  this.favoriteToy = favoriteToy;
+
+};
+
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
+const personTwo = new Baby({
+  name: 'Romeo',
+  age: 2,
+  favoriteToy:"stick"
+});
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. New binding uses "this" as a way to refer to a specifc item in contructor functions
+    2. Explicit binding is used with the call or apply method
+    3. Window object binding references the global scope
+    4. Implicit Binding is the most common use of "this" and it takes the function on the left as the reference
   */
   
   
