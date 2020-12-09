57dbdbaed3df40aa94bd6ba4d42a43e737f73d47
JoseLGuerra92 <joselguerra0@gmail.com>
2020-12-09 00:13:38 -0500

    Proto

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood);
   }
+}
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+}
  
  
 
@@ -63,9 +79,33 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance) {
+  // calculate how much distance is possible given our tank
+  let distancePossible = this.tank * this.milesPerGallon;
+  let distanceTraveled = distance;
+  if (distance > distancePossible) {
+    distanceTraveled = distancePossible;
   }
+  this.odometer += distanceTraveled;
+  // calculate gallons used to achieve distance
+  let gallonsUsed = distanceTraveled / this.milesPerGallon;
+  this.tank -= gallonsUsed;
+
+  if (this.tank === 0) {
+  return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+}
   
   
   /*
@@ -75,18 +115,26 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
+ function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+}
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}.`;
+}
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. global scope, the value of "this" will be the window/ console object
+    2. the object left of the dot, gets "this". calling an obj method
+    3. "this" refers to the specific instance of the obj that is created
+    4.  this is explicitly defined. 
   */
   
   
