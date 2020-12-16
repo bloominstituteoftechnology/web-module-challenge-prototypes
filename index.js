eaf948b35afab08f52069b9cf6e27cdf7fc2a5ff
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2020-12-16 14:22:45 -0500

    message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
   EXAMPLE TASK:
     - Write an Airplane constructor that initializes `name` from an argument.
     - All airplanes built with Airplane should initialize with an `isFlying` of false.
-    - Give airplanes the ability to `.takeOff()` and `.land()`:
+    - Give airplanes the ability to `.takeOff()` and `.land()`:lkqnlknsalknlksanlkasnlnsdla
         + If a plane takes off, its `isFlying` property is set to true.
         + If a plane lands, its `isFlying` property is set to false.
 */
@@ -39,10 +39,22 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age){
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
-
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  } 
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+return `${this.name} +", "+${this.age}`;
+}
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -57,9 +69,23 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon){
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function(gallons){
+  return this.tank += gallons;
+}
+Car.prototype.drive = function(distance){
+  this.odometer = distance;
+  this.tank -= distance/this.milesPerGallon;
+  if(this.tank === 0){
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+}
+
 
 /*
   TASK 3
@@ -68,18 +94,24 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
 
+Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
+  2. Whenever a function is contained in the global scope, the value of this inside of that function will be the window object.
+  3. Whenever JavaScript’s call or apply method is used, this is explicitly defined.
+  4. The "prototype" property points to the prototype of the instantiated object, but it is not a prototype of the constructor function.
 */
 
 
