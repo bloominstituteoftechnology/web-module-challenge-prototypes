26c84f4bf18eb5569371ce1b846025439626690f
Tee Chandy <73080544+tpchandy@users.noreply.github.com>
2021-01-20 00:06:21 -0500

    completed MVP tasks and answered questions in task 4 in the index.js file.

--- index.js
+++ index.js
@@ -39,14 +39,32 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) { //a constructor function with 3 properties-name, age and stomach(an empty array)
+  this.name = name
+  this.age = age
+  this.stomach = [];
   }
- 
- 
+Person.prototype.eat = function(snack){ /* an eat method to push item(snack) 
+  into the stomach array; once the stomach has 10 items in it, nothing can be fed into the stomach*/
+  if(this.stomach.length < 10){
+    this.stomach.push(snack);
+  }
+  }
+ Person.prototype.poop = function(){ // for instances for the person to poop......returns empty array.
+  this.stomach = [];
+  }
+ Person.prototype.toString = function(){  // method to string
+  return `${this.name}, ${this.age}`;
+  }
+ const personOne = new Person('Dan', 24);
+ const personTwo = new Person('Nathan', 22);
+ const personThree = new Person('Zoe', 21);
 
-  
-  
+ console.log(personOne.toString());
+ console.log(personTwo.toString());
+ console.log(personThree.toString());
+
+ 
   
   
   /*
@@ -63,9 +81,26 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {  //constructor that initializes model and milePerGallon from arguments - will initialize 
+  // with tank and odometer both at '0'.
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
   }
+  
+Car.prototype.fill = function(gallons) {  //Gives cars the ability to get fueled with a `.fill(gallons)` method. 
+  this.tank = this.tank + gallons;  //Adds the gallons to `tank`.
+  };
+  const myCar = new Car({
+    model: "Jaguar",
+    milesPerGallon: 18
+  });
+  console.log(myCar.tank);
+  myCar.fill(20);
+  console.log(myCar.tank);
+ 
+  
   
   
   /*
@@ -75,18 +110,33 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) { //created Baby constructor subclassing Person.
+    Person.call(this, name, age);
+    this.favoriteToy = favoriteToy; //added a third argument to initialize favoriteToy
   }
+  Baby.prototype = Object.create(Person.prototype);
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`; 
+  }
+  
+  const thisBaby = new Baby('Amy',2,'Pony');
+  console.log(thisBaby.toString());
+  thisBaby.eat("nuggets");
+  console.log(thisBaby.stomach);
+  console.log(thisBaby.play());
+/* 
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+1. In the global scope, "this" refers to the global object containing variables and functions that 
+   can be accessed anywhere.
+2. When followed by a dot, "this" refers to the object that is being accessed in the context of the function invocation.
+3. When using a function method such as .call or .apply, "this" is explicitly passed as a parameter 
+   i.e. "this" refers to the object that is passed when .call or .apply is invoked.
+4. In constructor functions, When a constructor function is invoked, "this" refers to the instance of the new object that is returned 
+   by the constructor function.
   */
   
   
