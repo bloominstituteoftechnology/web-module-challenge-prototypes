0a51d5f89b3858398635b6886d72083b927120a7
Greg Brewton <gbrewton1980@gmail.com>
2021-02-16 16:15:33 -0700

    Completes Task 2

--- index.js
+++ index.js
@@ -76,8 +76,18 @@ function Person(name, age) {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    Car.prototype.tank = 0;
+    Car.prototype.odometer = 0;
+    Car.prototype.fill = function(gallons) {
+        this.tank = this.tank + gallons;
+    }
+    Car.prototype.drive = function(distance) {
+        this.odometer = this.odometer + distance;
+        this.tank = this.tank - (distance / this.milesPerGallon)
+    }
 }
 
 
