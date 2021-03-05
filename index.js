5f30174f798fc7a65af20dd62f330190b1d36a87
Nicole Pizzano <nicolepizzano@Nicoles-iMac.local>
2021-03-04 21:30:49 -0500

    commit

--- index.js
+++ index.js
@@ -39,8 +39,21 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
+ function Person(name, age) {
+   this.name = name;
+   this.age = age;
+   this.stomach = [];
     
+  }
+
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+  }
+
+  Person.prototype.poop = function(){
+    return `${this.name}, ${this.age}`
   }
  
  
@@ -63,10 +76,16 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
+ function Car(model, milesPerGallon) {
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+   this.tank = 0;
+   this.odometer = 0;
     
   }
-  
+  Car.prototype.fill = function(gallons){
+    this.tank = gallons + this.tank;
+  }
   
   /*
     TASK 3
@@ -75,18 +94,24 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
+ function Baby(name, age,favoriteToy) {
+   Person.call(this, name, age, favoriteToy);
+   this.favoriteToy = favoriteToy;
    
   }
- 
+ Baby.prototype =Object.create(Person.prototype);
+ Baby.prototype.play = function(){
+   return `Playing with ${this.favoriteToy}`
+ }
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. window/ global object, when used in the global scope
+    2. implicit binding / the object before the dot recieves the "this" keyword.
+    
+    3. new binding / to be used with constructor functions
+    4. explicit binding / using the 'call' or 'apply' method - 'this' is explicity defined.
   */
   
   
