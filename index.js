2e3d1070976ba8d77e19b537168e5b20cb6ec0b5
Brandon Hamburg <brando.hamburg@gmail.com>
2021-03-15 18:40:42 -0700

    OG push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
     - All airplanes built with Airplane should initialize with an `isFlying` of false.
     - Give airplanes the ability to `.takeOff()` and `.land()`:
         + If a plane takes off, its `isFlying` property is set to true.
-        + If a plane lands, its `isFlying` property is set to false.
+        + If a plane lands, its `isFlying` property is set to false..
 */
 
 // EXAMPLE SOLUTION CODE:
