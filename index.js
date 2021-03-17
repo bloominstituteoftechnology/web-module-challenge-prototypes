17d3f5061e2ee58dc0d654ec5af188811e78100a
Matthew Norris <matthewnorris@Matthews-iMac.attlocal.net>
2021-03-16 20:55:00 -0500

    Finished work

--- index.js
+++ index.js
@@ -39,8 +39,27 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+   this.name = name;
+   this.age = age;
+   this.stomach = [];
+   this.toString = function() {
+     return  `${this.name}, ${this.age}`;
+   }
+ }
+
+  Person.prototype.eat = function(someFood) {
+     let foodCount = 0;
+     if(foodCount < 10) {
+      foodCount++;
+      this.stomach.push(someFood);
+     }
+  }
+  Person.prototype.poop = function() {
+     this.stomach = [];
+  }
+  Person.prototype.toString = function() {
+    console.log(`${this.name}, ${this.age}`);
   }
  
  
@@ -63,9 +82,23 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+   this.tank = 0;
+   this.odometer = 0;
   }
+
+  Car.prototype.fill = function(gallons) {
+    this.tank += gallons;
+  }
+
+  Car.prototype.drive = function(distance) {
+    this.tank -= distance/this.milesPerGallon;
+    this.odometer += distance;
+  }
+
+
   
   
   /*
@@ -75,18 +108,27 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   this.name = name;
+   this.age = age;
+   this.favoriteToy = favoriteToy;
+  }
+
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`
   }
  
+  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Implicit Binding with 'this' helps in function scope and just applies parameters set in function
+    2. Explicit is useful with prototypes for example
+    3. calling with this is useful to help apply things
+    4. this with a new binding helps apply meaning where needed
   */
   
   
