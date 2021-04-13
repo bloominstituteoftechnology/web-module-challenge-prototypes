cdc27dd79046d0ff9f369e2dfc08923d0b974de8
Amlak befekadu <amlakbarkon@gmail.com>
2021-04-13 11:37:16 -0400

    task 2 done working on 3

--- index.js
+++ index.js
@@ -39,29 +39,25 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person(name, age) {
-    this.name= name;
-    this.age = age;
-    this.stomach = [];
-   
-  
-  }
-  Person.prototype.eat = function(food){
-    return this.stomach.push(food)
-  }
-  Person.prototype.poop = function(){
-    return this.stomach= [];
-  }
-  Person.prototype.toString = function(){
-    return `${this.name} and ${this.age}`
-  }
- let alex = new Person("Alex", 45);
- alex.eat("Enjera");
- alex.poop();
- alex.toString();
- console.log(alex.eat("Hot Dog"))
- console.log(alex.stomach)
-  
+          function Person(name, age) {
+            this.name= name;
+            this.age = age;
+            this.stomach = [];
+           
+          
+          }
+          Person.prototype.eat = function(food){
+            return this.stomach.push(food)
+          }
+          Person.prototype.poop = function(){
+            return this.stomach= [];
+          }
+          Person.prototype.toString = function(){
+            return `${this.name} and ${this.age}`
+          }
+         let alex = new Person("Alex", 45);
+        
+          
   
   
   
@@ -79,11 +75,23 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+   this.tank = 0;
+   this.odometer = 0;
+   
   }
-  
-  
+  Car.prototype.fill = function(gallons){
+    return this.tank = this.tank + gallons;
+  }
+  Car.prototype.drive = function(distance){
+    this.odometer= this.odometer +1;
+    this.tank = this.tank - 1;
+  }
+   
+  const honda = new Car("Honda", "Corolla");
+
   /*
     TASK 3
       - Write a Baby constructor subclassing Person.
@@ -91,17 +99,25 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(favoriteToy) {
+   Person.call(this, favoriteToy);
+   this.favoriteToy = favoriteToy;
+  
+  }
+  Baby.prototype = Object.create(Person.prototype)
+  Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
   }
  
-  
+ let barkon = new Baby("soccer");
+ barkon.play();
+
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
+    1. window object when this key word by it self  
+    2. implicit when constructor function hold all the methods and the properties. method and properties listed on the parent constructor function 
+    3. explicit when functions are stand by their own and call onto object with call bind and apply.
     4. 
   */
   
