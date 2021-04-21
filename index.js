63969eb844819ac7dcd86b67c5642fc8831d6f77
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-04-21 15:34:51 -0400

    Task 2 Completed

--- index.js
+++ index.js
@@ -57,13 +57,6 @@ function Airplane(name) {
     return `${this.name}, ${this.age}`
   }
 
- 
- 
-
-  
-  
-  
-  
   /*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -78,8 +71,14 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons;
   }
   
   
