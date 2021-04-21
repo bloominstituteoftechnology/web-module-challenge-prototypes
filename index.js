50951dcfd118908c715a70420f39fc0815ec9fd2
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-04-21 14:39:52 -0400

    Task one completed

--- index.js
+++ index.js
@@ -39,9 +39,24 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name,age) {
+   this.name = name;
+   this.age = age;
+   this.stomach = [];
   }
+
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    };
+  }
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  }
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`
+  }
+
  
  
 
