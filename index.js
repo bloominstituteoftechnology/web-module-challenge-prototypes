4b5fb1b6f9a9d2bd801357cce25209481c612cf8
Mykael Barnes <mykael.barnes@hotmail.com>
2021-05-11 14:31:17 -0700

    finished project

--- index.js
+++ index.js
@@ -39,15 +39,26 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name,
+    this.age = age,
+    this.stomach = [];
+  }
+
+  Person.prototype.eat = function(food) {
+    if (this.stomach.length < 10) {
+        this.stomach.push(food);
+      }
+  }
+
+  Person.prototype.poop = function() {
+    this.stomach.splice(0, this.stomach.length);
+  }
+
+  Person.prototype.toString = function() {
+    return `${this.name}, ${this.age}`;
   }
  
- 
-
-  
-  
-  
   
   /*
     TASK 2
@@ -63,8 +74,15 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
+  }
+
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons;
   }
   
   
@@ -75,18 +93,27 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy
    
   }
+
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`;
+  }
+
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Global binding. In this context, this is referring to the console.
+    2. Implicit binding. The context for this would be the object before the ".method".
+    3. New binding. This creates a new instance of an object from a constructor and the context would be the constructor.
+    4. Explicit binding. Through using "call", and "apply" you can explicitly define "this"
   */
   
   
