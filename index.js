34bd3952277fa9aa6ce9d49754a572863e72d440
Debra <debrafon@gmail.com>
2021-05-11 21:38:42 -0400

    car filled up

--- index.js
+++ index.js
@@ -39,10 +39,23 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
- 
+
+  Person.prototype.eat = function (someFood) {
+    if (this.somtach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+//  Person.prototype.poop = fuction () {
+//    this.stomach = []
+//  }
+//   Person.prototype.toString = function () {
+//     return `${this.name}, ${this.age}`
+//   }
  
 
   
@@ -63,11 +76,20 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
+ function Car(model, milesPerGallon) {
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+   this.tank = 0;
+   this.odometer = 0;
+  
     
   }
   
-  
+  Car.prototype.fill = function (gallons) {
+    this.tank = this.tank + gallons
+  }
+
+
   /*
     TASK 3
       - Write a Baby constructor subclassing Person.
