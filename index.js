6fb360a2823116d66e14786f5a94f0d9dd9ba34d
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-05-11 18:57:53 -0700

    Finished problems with a litte help, going back for explanations

--- index.js
+++ index.js
@@ -79,11 +79,16 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
   
-  
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons;
+  }
   /*
     TASK 3
       - Write a Baby constructor subclassing Person.
@@ -91,11 +96,18 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
+ function Baby(name, age, favoriteToy) {
+   this.name = name;
+   this.age = age;
+   this.favoriteToy = favoriteToy;
    
   }
  
-  
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`;
+  }
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
