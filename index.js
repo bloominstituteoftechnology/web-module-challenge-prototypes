145502e10e683e97500f6d1c4644bfc290f561ee
jose-mercado <josefmercado99@gmail.com>
2021-05-11 22:13:54 -0700

    Finished Project

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function Airplane(name) {
   };
   Airplane.prototype.land = function () {
     this.isFlying = false;
-  };
+  }
   
   
   /*
@@ -28,62 +28,93 @@ function Airplane(name) {
   
   /*
     TASK 1
-      - Write a Person Constructor that initializes `name` and `age` from arguments.
-      - All instances of Person should initialize with an empty `stomach` array.
-      - Give instances of Person the ability to `.eat("someFood")`:
-          + When eating an edible, it should be pushed into the `stomach`.
-          + The `eat` method should have no effect if there are 10 items in the `stomach`.
-      - Give instances of Person the ability to `.poop()`:
-          + When an instance poops, its `stomach` should empty.
-      - Give instances of Person a method `.toString()`:
-          + It should return a string with `name` and `age`. Example: "Mary, 50"
+      [x]- Write a Person Constructor that initializes `name` and `age` from arguments.
+      [x]- All instances of Person should initialize with an empty `stomach` array.
+      [x]- Give instances of Person the ability to `.eat("someFood")`:
+          [x]+ When eating an edible, it should be pushed into the `stomach`.
+          [x]+ The `eat` method should have no effect if there are 10 items in the `stomach`.
+      [x]- Give instances of Person the ability to `.poop()`:
+          [x]+ When an instance poops, its `stomach` should empty.
+      [x]- Give instances of Person a method `.toString()`:
+          [x]+ It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+  function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach=[];
   }
- 
- 
 
+Person.prototype.eat = function (someFood) {
+  if (this.stomach.length <= 10) {
+    this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function () {
+  this.stomach = []
+}
+
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`;
+} 
   
   
   
   
   /*
     TASK 2
-      - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
-      - All instances built with Car:
-          + should initialize with an `tank` at 0
-          + should initialize with an `odometer` at 0
-      - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-      - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
+      [x]- Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+      [x]- All instances built with Car:
+          [x]+ should initialize with an `tank` at 0
+          [x]+ should initialize with an `odometer` at 0
+      [x]- Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+      -STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
           + Should cause the `odometer` to go up.
           + Should cause the the `tank` to go down taking `milesPerGallon` into account.
       - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+  function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
   
-  
+  Car.prototype.fill = function (gallons) {
+    this.tank = this.tank + gallons;
+  }
+
+
+
   /*
     TASK 3
-      - Write a Baby constructor subclassing Person.
-      - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
-      - Besides the methods on Person.prototype, babies have the ability to `.play()`:
-          + Should return a string "Playing with x", x being the favorite toy.
+      - [x]Write a Baby constructor subclassing Person.
+      - [x]Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
+      - [x]Besides the methods on Person.prototype, babies have the ability to `.play()`:
+          [x]+ Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+
+
+  Baby.prototype = Object.create(Person.prototype)
+
+  function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age);
+    this.favoriteToy = favoriteToy;
   }
- 
+
+    Baby.prototype.play = function () {
+      return `Playing with ${this.favoriteToy}`;
+    }
+
+
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
+    1.  
     2. 
     3. 
     4. 
