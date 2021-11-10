4d087ae8cdc9daa48f2ef3042b949f8cf288dac9
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2021-11-10 15:51:12 -0600

    finihsed mvp

--- index.js
+++ index.js
@@ -39,14 +39,28 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function (){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
-
+const gio = new Person ('Gio', 19);
+console.log('task 1:' ,gio.toString())
 
 
 /*
@@ -63,10 +77,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model,  milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;  
 }
-
+Car.prototype.fill = function(gallons) {
+ this.tank = this.tank + gallons 
+}
 
 /*
   TASK 3
@@ -75,18 +94,22 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ Person.call(this, name,age);
+ this.favoriteToy = favoriteToy
 }
 
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Binding is an errror when none of the other princples apply it will return global object or undefined
+  2. Implicit is when the function to the left of the dot is the this keyword
+  3. Explicit is when u use call and bind and apply
+  4. New Binding is created a new object in a constructor func
 */
 
 
