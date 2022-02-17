24a34f2e5e14fa2bdedfa456b3069ed9376ed598
Aviance Obie <americancliterature@Aviances-MBP.attlocal.net>
2022-02-16 18:04:18 -0600

    practiced prototypes and inheritance

--- index.js
+++ index.js
@@ -39,15 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name
+  this.age = age
+  this.stomach = []
 }
 
+Person.prototype.eat = function (food) {
+  if (this.stomach.length < 10) {
+  this.stomach.push(food)
+  } return this.stomach
+}
 
-
-
-
-
+Person.prototype.poop = function () {
+  this.stomach = []
+  return this.stomach
+}
+  
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`
+}
 
 /*
   TASK 2
@@ -63,10 +74,17 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
 }
 
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons
+  return this.tank
+}
 
 /*
   TASK 3
@@ -75,18 +93,27 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.name = name
+  this.age = age
+  this.favoriteToy = favoriteToy
+}
+
+Baby.prototype = Object.create(Person.prototype)
+
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Object Binding: In the global scope, 'this' is the window/console object.
+  2. Implicit Binding: 'This' is implicitly defined when using a dot calls a function.
+  3. New Binding: In a constructor function, 'this' is the instance of the object created by the function.
+  4. Explicit Binding: 'This' is explicitly defined when using a 'call' or 'apply' method.
 */
 
 
